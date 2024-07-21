54c974f41b9f9ffea5dca42147392098acdc914b
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2024-07-22 00:54:43 +0100

    move gui to the separate class

--- src/verlet_fast/code/private/verlet_app.cpp
+++ src/verlet_fast/code/private/verlet_app.cpp
@@ -1,15 +1,11 @@
 #include "verlet_app.hpp"
 
-#include <thread>
-
 #include "EverydayTools/Time/MeasureTime.hpp"
 #include "coloring/spawn_color/spawn_color_strategy_rainbow.hpp"
-#include "coloring/tick_color/tick_color_strategy_velocity.hpp"
-#include "imgui_helpers.hpp"
+#include "coloring/tick_color/tick_color_strategy.hpp"
+#include "gui/app_gui.hpp"
 #include "klgl/opengl/debug/annotations.hpp"
 #include "klgl/opengl/gl_api.hpp"
-#include "ranges.hpp"
-#include "tools/delete_objects_tool.hpp"
 #include "tools/move_objects_tool.hpp"
 #include "tools/spawn_objects_tool.hpp"
 
@@ -129,7 +125,7 @@ void VerletApp::UpdateSimulation()
 void VerletApp::Render()
 {
     RenderWorld();
-    RenderGUI();
+    AppGUI{*this}.Render();
 }
 
 void VerletApp::RenderWorld()
@@ -173,189 +169,4 @@ void VerletApp::RenderWorld()
         });
 }
 
-void VerletApp::RenderGUI()
-{
-    auto to_flt_ms = [](auto duration)
-    {
-        return std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(duration);
-    };
-
-    const klgl::ScopeAnnotation annotation("Render GUI");
-    if (ImGui::Begin("Settings"))
-    {
-        shader_->DrawDetails();
-        GuiText("Framerate: {}", GetFramerate());
-        GuiText("Objects count: {}", solver.objects.ObjectsCount());
-        GuiText("Sim update {}", to_flt_ms(perf_stats_.sim_update.total));
-        GuiText("  Apply links {}", to_flt_ms(perf_stats_.sim_update.apply_links));
-        GuiText("  Rebuild grid {}", to_flt_ms(perf_stats_.sim_update.rebuild_grid));
-        GuiText("  Solve collisions {}", to_flt_ms(perf_stats_.sim_update.solve_collisions));
-        GuiText("  Update positions {}", to_flt_ms(perf_stats_.sim_update.update_positions));
-        GuiText("Render {}", to_flt_ms(perf_stats_.render.total));
-        GuiText("  Set Circle Loop {}", to_flt_ms(perf_stats_.render.set_circle_loop));
-        if (ImGui::CollapsingHeader("Emitter"))
-        {
-            ImGui::Checkbox("Enabled", &enable_emitter_);
-            if (enable_emitter_)
-            {
-                ImGuiHelper::SliderUInt("Max objects", &emitter_max_objects_count_, 0uz, 150'000uz);
-            }
-        }
-        GUI_Tools();
-        GUI_SpawnColors();
-        GUI_TickColors();
-
-        if (ImGui::CollapsingHeader("Collisions Solver"))
-        {
-            size_t count = solver.GetThreadsCount();
-            GuiText("0 threads means collisions will be solved in the main thread");
-            if (ImGuiHelper::SliderUInt(
-                    "Threads Count",
-                    &count,
-                    size_t{0},
-                    size_t{std::thread::hardware_concurrency()}))
-            {
-                solver.SetThreadsCount(count);
-            }
-        }
-    }
-    ImGui::End();
-
-    if (ImGui::Begin("Cells"))
-    {
-        for (const auto& [cell_index, cell_objects_count] : Enumerate(solver.cell_obj_counts_))
-        {
-            if (cell_objects_count != 0)
-            {
-                const size_t cell_x = cell_index % solver.grid_size_.x();
-                const size_t cell_y = cell_index / solver.grid_size_.x();
-                GuiText("Cell {} {}: {}", cell_x, cell_y, cell_objects_count);
-            }
-        }
-    }
-    ImGui::End();
-}
-
-void VerletApp::GUI_Tools()
-{
-    auto use_tool = [&](const ToolType tool_type)
-    {
-        if (!tool_ || tool_->GetToolType() != tool_type)
-        {
-            switch (tool_type)
-            {
-            case ToolType::SpawnObjects:
-                tool_ = std::make_unique<SpawnObjectsTool>(*this);
-                break;
-            case ToolType::MoveObjects:
-                tool_ = std::make_unique<MoveObjectsTool>(*this);
-                break;
-            case ToolType::DeleteObjects:
-                tool_ = std::make_unique<DeleteObjectsTool>(*this);
-                break;
-            default:
-                tool_ = nullptr;
-                break;
-            }
-        }
-
-        if (tool_)
-        {
-            tool_->DrawGUI();
-        }
-    };
-
-    ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None | ImGuiTabBarFlags_DrawSelectedOverline;
-    if (ImGui::BeginTabBar("Tools", tab_bar_flags))
-    {
-        if (ImGui::BeginTabItem("Spawn"))
-        {
-            use_tool(ToolType::SpawnObjects);
-            ImGui::EndTabItem();
-        }
-        if (ImGui::BeginTabItem("Move"))
-        {
-            use_tool(ToolType::MoveObjects);
-            ImGui::EndTabItem();
-        }
-        if (ImGui::BeginTabItem("Delete"))
-        {
-            use_tool(ToolType::DeleteObjects);
-            ImGui::EndTabItem();
-        }
-
-        ImGui::EndTabBar();
-    }
-}
-
-void VerletApp::GUI_SpawnColors()
-{
-    auto use = [&](const cppreflection::Type* type)
-    {
-        if (&spawn_color_strategy_->GetType() != type)
-        {
-            if (type == cppreflection::GetTypeInfo<SpawnColorStrategyRainbow>())
-            {
-                spawn_color_strategy_ = std::make_unique<SpawnColorStrategyRainbow>(*this);
-            }
-        }
-
-        spawn_color_strategy_->DrawGUI();
-    };
-
-    if (ImGui::CollapsingHeader("Spawn Color"))
-    {
-        ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None | ImGuiTabBarFlags_DrawSelectedOverline;
-        if (ImGui::BeginTabBar("Spawn Color", tab_bar_flags))
-        {
-            if (ImGui::BeginTabItem("Rainbow"))
-            {
-                use(cppreflection::GetTypeInfo<SpawnColorStrategyRainbow>());
-                ImGui::EndTabItem();
-            }
-
-            ImGui::EndTabBar();
-        }
-    }
-}
-
-void VerletApp::GUI_TickColors()
-{
-    auto use = [&](const cppreflection::Type* type)
-    {
-        if (!tick_color_strategy_ || &tick_color_strategy_->GetType() != type)
-        {
-            if (type == cppreflection::GetTypeInfo<TickColorStrategyVelocity>())
-            {
-                tick_color_strategy_ = std::make_unique<TickColorStrategyVelocity>(*this);
-            }
-        }
-
-        if (tick_color_strategy_)
-        {
-            tick_color_strategy_->DrawGUI();
-        }
-    };
-
-    if (ImGui::CollapsingHeader("Tick Color"))
-    {
-        ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None | ImGuiTabBarFlags_DrawSelectedOverline;
-        if (ImGui::BeginTabBar("Tick Color", tab_bar_flags))
-        {
-            if (ImGui::BeginTabItem("None"))
-            {
-                tick_color_strategy_ = nullptr;
-                ImGui::EndTabItem();
-            }
-
-            if (ImGui::BeginTabItem("Velocity"))
-            {
-                use(cppreflection::GetTypeInfo<TickColorStrategyVelocity>());
-                ImGui::EndTabItem();
-            }
-
-            ImGui::EndTabBar();
-        }
-    }
-}
 }  // namespace verlet
